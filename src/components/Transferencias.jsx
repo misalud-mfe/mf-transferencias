c0b035b41a4daf44f093b9a28a6dec94a13c0676
haroldevs <haroldthomasg18@gmail.com>
2025-06-01 23:46:08 -0500

    chore: tabla de transferencias

--- src/components/Transferencias.jsx
+++ src/components/Transferencias.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import "common-resources/css/table.css";
 
 const Transferencias = () => {
+  const [transferidos, setTransferidos] = useState([]);
+
+  useEffect(() => {
+    if (window.transferidos) {
+      setTransferidos(window.transferidos);
+    }
+  }, []);
+
   return (
-    <div>Transferencias</div>
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Paciente</th>
+            <th>Historia</th>
+            <th>Prioridad</th>
+            <th>Destino</th>
+            <th>Fecha</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transferidos.map((a) => (
+            <tr key={a.atencionId}>
+              <td>{a.nombreCompleto}</td>
+              <td>{a.historiaClinica}</td>
+              <td style={{ backgroundColor: a.prioridadBgColor, color: a.prioridadFgColor }}>
+                {a.prioridad}
+              </td>
+              <td>{a.destinoAtencion}</td>
+              <td>{new Date(a.createdAt).toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
